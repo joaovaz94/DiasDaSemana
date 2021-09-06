79d3b22513b1f18b39aca910669b87d89e716398
joaovaz94 <j>
2021-09-06 18:23:34 -0300

    Trabalho 5 finalizado

--- Main.java
+++ Main.java
@@ -6,46 +6,50 @@ import java.util.stream.*;
 public class Main {
 
 	public static void main(String[] args) {
-
-		var argsLista = new ArrayList<String>(Arrays.asList(args));
-		//int arrayInteiros[] = new int [args.length];
-		//int i = 0;
-		/*
-		for(String a : args){
-			try {
-				//converter string pra int
-				int numeroPassado = Integer.parseInt(a);
-				//verificar se é um número válido (1 a 7)
-				if(numeroPassado > 0 && numeroPassado <= 7) {
-					arrayInteiros[i] = numeroPassado;
-				}
-				else {
-					System.out.println("O número: " + numeroPassado + " não é um número válido (inteiros entre 1 e 7) ");
-				}
-			} catch (NumberFormatException e) {
-				System.out.println("Não foi passado um número inteiro como argumento!");
-			}
-			i++;
-		}
-		*/
-
-		//List<Integer> dias = argsLista
-		argsLista
-			.stream()
+		
+		System.out.println("\nElementos passados como argumento que não são números inteiros: ");
+		Stream
+			.of(args)
+			.filter(Main::verificaNaoInteiro)
+			.forEach(d -> System.out.println(d));
+		
+		System.out.println("\nElementos passados como argumento que não são números inteiros entre 1 e 7: ");
+		Stream
+			.of(args)
+			.filter(Main::verificaInteiro)
 			.map(Integer::valueOf)
-			.filter(d -> d instanceof Integer)
-			.filter(d -> d > 1 && d <= 7)
+			.filter(d -> d > 7 || d < 0)
+			.forEach(d -> System.out.println(d.toString()));
+
+		System.out.println("\nDescrição de dias da semana: ");
+		Stream
+			.of(args)
+			.filter(Main::verificaInteiro)
+			.map(Integer::valueOf)
+			.distinct()
+			.filter(d -> d > 0 && d < 8)
 			.map(DiaDaSemana::new)
-			.forEach(d -> System.out.println(d.getDescricao()));
+			.forEach(d -> System.out.println(d.toString()));
 
-					//.collect(Collectors.toList());
-		//System.out.println(argsLista);
-		
-		//iterar no array imprimindo o getDescricao()
+	}
 
-			//implementar o getDescricao com um switch/case para imprimir o nome do dia da semana
-		
-		//DescreverDiaDaSemana d = new DescreverDiaDaSemana();
-		//d.executar(arrayInteiros);
+	public static boolean verificaInteiro(String entrada) {
+		try {
+			Integer.valueOf(entrada);
+			return true;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	public static boolean verificaNaoInteiro(String entrada) {
+		try {
+			Integer.valueOf(entrada);
+			return false;
+		}
+		catch (NumberFormatException e) {
+			return true;
+		}
 	}
 }
--- biblioteca/dominio/DiaDaSemana.java
+++ biblioteca/dominio/DiaDaSemana.java
@@ -46,4 +46,47 @@ public class DiaDaSemana {
 		return "O dia da semana é: " + dia;
 		
 	}
+
+	public String toString() {
+		
+		String dia = new String();
+		switch(indice) {
+			case 1:
+				dia = "Domingo";
+				break;
+			case 2:
+				dia = "Segunda";
+				break;
+			case 3:
+				dia = "Terça";
+				break;
+			case 4:
+				dia = "Quarta";
+				break;
+			case 5:
+				dia = "Quinta";
+				break;
+			case 6:
+				dia = "Sexta";
+				break;
+			case 7:
+				dia = "Sábado";
+				break;
+		}
+		return "O dia da semana é: " + dia;
+	}
+
+	public boolean equals(Object o) {
+		if(!(o instanceof DiaDaSemana)) {
+			return false;
+		}
+		DiaDaSemana d = (DiaDaSemana) o;
+
+		if(d.indice == this.indice) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
